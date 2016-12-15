7ffc452f41d0b673a50ca6466b33f735d19f8eda
cezarsantanna <cezar.santanna@gmail.com>
2016-12-14 22:27:57 -0200

    Version 0.1.0.0.0

--- TemosPortal/urls.py
+++ TemosPortal/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^', include('abastece.urls')),
     url(r'^abastece/', include('abastece.urls')),
     url(r'^admin/', admin.site.urls),
 ]
