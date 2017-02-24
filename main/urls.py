b4aaa8f552789678b74f5926286bcc265def45d4
cezarsantanna <cezar.santanna@gmail.com>
2017-02-24 17:41:35 -0300

    Autenticação no Site

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^search/$', views.BuscaPosto.as_view(), name='search'),
+    url(r'^accounts/login/$', auth_views.login, {'template_name': 'main/login.html'}, name='login'),
+    url(r'^accounts/logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
 ]
